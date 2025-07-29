f808955d3a0fc2080972f49adcd4e1fd4271c17f
corelord-surf <dunn.nathan@hotmail.com>
2025-07-30 06:34:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const msalConfig = {
+  auth: {
+    clientId: "825d8657-c509-42b6-9107-dd5e39268723",
+    authority: "https://login.microsoftonline.com/d048d6e2-6e9f-4af0-afcf-58a5ad036480",
+    redirectUri: "https://agreeable-ground-04732bc03.1.azurestaticapps.net"
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+};
+
+let msalInstance;
+if (window.msal) {
+  msalInstance = new msal.PublicClientApplication(msalConfig);
+} else {
+  console.error("MSAL is not loaded. Make sure to include the MSAL script in your HTML.");
+}
+
 async function signIn() {
   try {
     const loginResponse = await msalInstance.loginPopup({
